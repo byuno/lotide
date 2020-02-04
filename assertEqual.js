adf40a257f470538b75a91872e5e3f47ae652a35
Bobby Uno <robert.y.uno@gmail.com>
2020-02-04 02:55:06 +0000

    Completed assertEqual with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,15 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟢🟢🟢 Assertion Passed: " + actual + " ==== " + expected);
+    console.log(`🟢🟢🟢 Assertion Passed:  ${actual} ==== ${expected}`);
   } else {
-    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔴🔴🔴 Assertion Failed:  ${actual} !== ${expected}`);
   }
 
 };
 
 // TEST CODE
-assertEqual(["Lighthouse Labs"], ["Bootcamp"]);
+assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("string", "string");
 assertEqual(12, 234);
