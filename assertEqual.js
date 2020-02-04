8a37eabfbf590fc81b80cde3a74747d4a99d439c
Bobby Uno <robert.y.uno@gmail.com>
2020-02-04 02:41:49 +0000

    Completed assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: " + actual + " ==== " + expected);
+  } else {
+    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual(["Lighthouse Labs"], ["Bootcamp"]);
+assertEqual(1, 1);
+assertEqual("string", "string");
+assertEqual(12, 234);
