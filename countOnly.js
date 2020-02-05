7a6aeb39c65387a854d6deab0e20c8e8849c59d9
Bobby Uno <robert.y.uno@gmail.com>
2020-02-05 21:06:32 +0000

    countOnly.js created

---
+++ countOnly.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed:  ${actual} ==== ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+
+};
