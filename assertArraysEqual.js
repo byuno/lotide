6de659de42067eb9aac023059a76f349241621cb
Bobby Uno <robert.y.uno@gmail.com>
2020-02-05 03:10:14 +0000

    Completed assertArraysEqual assignment

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const assertArrayEqual = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    console.log(`🔴🔴🔴 Assertion Failed:  ${array1} !== ${array2}`);
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🔴🔴🔴 Assertion Failed:  ${array1} !== ${array2}`);
+      return false
+    }
+  }
+
+  console.log(`🟢🟢🟢 Assertion Passed:  ${array1} ==== ${array2}`);
+
+  return true;
+};
+
+assertArrayEqual([1, 2, 3], [1, 2, "z"]);
+
