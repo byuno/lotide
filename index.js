0e92eceee712d6481f40e996e9660959dca05ab8
Bobby Uno <robert.y.uno@gmail.com>
2020-02-11 02:25:43 +0000

    updated index.js

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+
+
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays
 };
